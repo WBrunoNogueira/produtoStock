7116d4b5d4c545bef90bfa80b9fd734553b1cde4
SK1PP3R <wendersonbruno10@gmail.com>
2026-07-10 17:09:44 -0300

    Initial commit

---
+++ src/main/java/br/com/produtoStock/ProdutoStockApplication.java
@@ -0,0 +1,13 @@
+package br.com.produtoStock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutoStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutoStockApplication.class, args);
+	}
+
+}
