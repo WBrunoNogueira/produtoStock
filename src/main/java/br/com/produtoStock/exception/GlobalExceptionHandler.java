798ab480af309d3fd69296d9fc47b765e4d628f3
Wenderson Bruno Alves Nogueira <wendersonbruno10@gmail.com>
2026-07-22 17:27:18 -0300

    feat: adc tratamento de erros de validacão

--- src/main/java/br/com/produtoStock/exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/produtoStock/exception/GlobalExceptionHandler.java
@@ -2,10 +2,12 @@ package br.com.produtoStock.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
@@ -15,7 +17,7 @@ public class GlobalExceptionHandler {
     public ResponseEntity<Map<String, Object>> handleProductNotFound(
             ProductNotFoundException exception
     ) {
-
+        // trata produto não encontrado
         Map<String, Object> error = Map.of(
                 "status", HttpStatus.NOT_FOUND.value(),
                 "error", "Not Found",
@@ -26,5 +28,24 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(error);
+    }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationErrors(
+            MethodArgumentNotValidException exception
+    ) {
+        // trata campos inválidos
+        Map<String, String> errors = new HashMap<>();
+
+        exception.getBindingResult()
+                .getFieldErrors()
+                .forEach(error ->
+                        errors.put(error.getField(), error.getDefaultMessage())
+                );
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(errors);
     }
 }
